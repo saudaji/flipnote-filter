98653769091dece434efe80fa89324f68bbe3608
saudaji <santiagotejedasolis@gmail.com>
2026-03-21 00:48:33 -0600

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flipnote-filter-v7';
+const CACHE = 'flipnote-filter-v8';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e =>

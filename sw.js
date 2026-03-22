a79a439a0855a9bfc8cfb50c3a6ce33d85c90534
saudaji <santiagotejedasolis@gmail.com>
2026-03-22 16:41:36 -0600

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flipnote-filter-v43';
+const CACHE = 'flipnote-filter-v44';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './fonts/Bloom-Regular.otf', './fonts/BNMonica.otf', './fonts/Fluidic-Regular.otf', './fonts/Canterbury.ttf'];
 

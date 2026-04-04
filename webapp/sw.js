152240f9d0f6701e0d39638bf69cfca68ca3b98d
Sagredo <tu-email@gmail.com>
2026-04-04 16:03:16 -0600

    Stabilize camera UI overlays and engine capture flow

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flipnote-filter-v51';
+const CACHE = 'flipnote-filter-v52';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './fonts/Bloom-Regular.otf', './fonts/BNMonica.otf', './fonts/Fluidic-Regular.otf', './fonts/Canterbury.ttf'];
 
